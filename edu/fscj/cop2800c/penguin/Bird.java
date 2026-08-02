3d92ba134bcf93af90e0b749a74ef6daae19651c
latoyasmothers-tolar-bit <lavirgo0318@yahoo.com>
2026-08-02 15:38:48 -0400

    Bird.java

--- edu/fscj/cop2800c/penguin/Bird.java
+++ edu/fscj/cop2800c/penguin/Bird.java
@@ -1,6 +1,6 @@
 // Bird.java
-// D. Singletary
-// 3/15/25
+// Latoya Smothers-Tolar
+// 08/02/2026
 // Represents a bird base class
 
 package edu.fscj.cop2800c.penguin;
@@ -16,7 +16,16 @@ public class Bird implements Comparable<Bird> {
 
     // Constructor
     public Bird(int sampleNum, double culmenLength, 
-                double culmenDepth, double bodyMass, String sex) {
+                double culmenDepth, double bodyMass, String sex)
+                throws InvalidBirdDataException {
+        if (sampleNum < 0 ||
+            culmenLength < 0 ||
+            culmenDepth < 0 ||
+            bodyMass < 0 ||
+            sex == null ||
+            sex.isEmpty()) {
+            throw new InvalidBirdDataException("Invalid Bird data encountered.");
+        }
         this.sampleNum = sampleNum;
         this.culmenLength = culmenLength;
         this.culmenDepth = culmenDepth;
